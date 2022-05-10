6fa09407ed68edd0e1580dfc503781666825b749
Сигов Дмитрий <99177871+SigovDmitriy@users.noreply.github.com>
2022-05-10 17:13:39 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,58 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+Train::Cage* Train::create(bool light) {
+    Cage* cage = new Cage;
+    cage->light = light;
+    cage->next = nullptr;
+    cage->prev = nullptr;
+    return cage;
+}
+void Train::addCage(bool light) {
+    if (last && first) {
+        last->next = create(light);
+        last->next->prev = last;
+        last = last->next;
+        first->prev = last;
+        last->next = first;
+    } else {
+        first = create(light);
+        last = first;
+    }
+}
+int Train::getLength() {
+    int len = 0;
+    first->light = true;
+    Cage* temp = first;
+    while (true) {
+        len = len + 1;
+        for (int s = 0; s < len; s++) {
+            temp = temp->next;
+            countOp = countOp + 1;
+        }
+        if (temp->light) {
+            temp->light = false;
+            for (int s = 0; s < len; s++) {
+                temp = temp->prev;
+                countOp = countOp + 1;
+            }
+        } else {
+            while (!temp->light) {
+                len = len + 1;
+                temp = temp->next;
+                countOp = countOp + 1;
+            }
+            temp->light = false;
+            for (int s = 0; s < len; s++) {
+                temp = temp->prev;
+                countOp = countOp + 1;
+            }
+        }
+        if (!temp->light) {
+            break
+        }
+    }
+    return len;
+}
+int Train::getOpCount() {
+    return countOp;
+}
